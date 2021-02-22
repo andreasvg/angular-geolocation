0c59fe8841164066789208022102420d16efe893
Andreas van Greunen <andreas.van.greunen@gmail.com>
2021-02-22 15:33:30 +0000

    Added feature to wath the user's position

--- src/app/geolocation.service.ts
+++ src/app/geolocation.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { share } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GeolocationService {
+  private geoOptions = {
+    enableHighAccuracy: true, // enable if available
+    timeout: 10000,           // 10 seconds
+    maximumAce: 300000        // only accept cached positions with this max
+  };
+
+  private watchId: number;
 
   public getPosition(): Observable<Position> {
 
@@ -15,10 +23,65 @@ export class GeolocationService {
           subscriber.next(location);
           subscriber.complete();
         },
-        err => subscriber.error(err)
+        err => subscriber.error(this.locationError(err)),
+        this.geoOptions
       );
 
     });
 
   }
+
+  public startWatching(): Observable<Position> {
+
+    return new Observable<Position>((subscriber) => {
+
+      this.watchId = navigator.geolocation.watchPosition(
+        location =>  {
+          subscriber.next(location);
+        },
+        err => subscriber.error(this.locationError(err)),
+        this.geoOptions
+      );
+
+      return () => {
+        console.log('about to tear down');
+        this.stopWatching();
+      }
+
+    }).pipe(
+      share()
+    );
+
+  }
+
+  private stopWatching(): void {
+    if (this.watchId) {
+      console.log('tearing down');
+      navigator.geolocation.clearWatch(this.watchId);
+    }
+  }
+
+  private locationError(error) {
+    let msg = "";
+
+    console.log("error.message = " + error.message);
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        msg = "User does not want to display their location.";
+        break;
+      case error.POSITION_UNAVAILABLE:
+        msg = "Can't determine user's location.";
+        break;
+      case error.TIMEOUT:
+        msg = "The request for geolocation information timed out.";
+        break;
+      case error.UNKNOWN_ERROR:
+        msg = "An unknown error occurred.";
+        break;
+    }
+
+    return msg;
+  }
+
+
 }
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -8,24 +8,59 @@ import { GeolocationService } from '../geolocation.service';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit, OnDestroy {
+  public watchingActive: boolean = false;
   private locationSubscription: Subscription;
   public latitude: number;
   public longitude: number;
+  public timestamp: number;
+  public accuracy: number;
+  public altitude: number;
+  public altitudeAccuracy: number;
+  public heading: number;
+  public speed: number;
 
   constructor(private geolocationService: GeolocationService) { }
 
 
   ngOnInit(): void {
-
-    this.locationSubscription = this.geolocationService.getPosition().subscribe(
-      location => {
-        console.log(location);
-        this.latitude = location.coords.latitude;
-        this.longitude = location.coords.longitude;
-      }
-    );
+    // this.locationSubscription = this.geolocationService.getPosition().subscribe(
+    //   location => {
+    //     console.log(location);
+    //     this.timestamp = location.timestamp;
+    //     this.latitude = location.coords.latitude;
+    //     this.longitude = location.coords.longitude;
+    //     this.accuracy = location.coords.accuracy;
+    //     this.altitude = location.coords.altitude;
+    //     this.altitudeAccuracy = location.coords.altitudeAccuracy;
+    //     this.heading = location.coords.heading;
+    //     this.speed = location.coords.speed;
+    //   }
+    // );
   }
 
+  public onStartWatchingClick(): void {
+    this.locationSubscription = this.geolocationService.startWatching().subscribe(
+      location => {
+        console.log(location);
+        this.timestamp = location.timestamp;
+        this.latitude = location.coords.latitude;
+        this.longitude = location.coords.longitude;
+        this.accuracy = location.coords.accuracy;
+        this.altitude = location.coords.altitude;
+        this.altitudeAccuracy = location.coords.altitudeAccuracy;
+        this.heading = location.coords.heading;
+        this.speed = location.coords.speed;
+      }
+    );
+    this.watchingActive = true;
+  }
+
+  public onStopWatchingClick(): void {
+    if (this.locationSubscription) {
+      this.locationSubscription.unsubscribe();
+    }
+    this.watchingActive = false;
+  }
 
   ngOnDestroy(): void {
     if (this.locationSubscription) {
